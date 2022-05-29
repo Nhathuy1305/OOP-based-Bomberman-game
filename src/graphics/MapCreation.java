d211852315cd94ae282d06b0aa839721dd1b99d3
Dang Nhat Huy <89309283+Nhathuy1305@users.noreply.github.com>
2022-05-30 00:18:51 +0700

    Update MapCreation.java file

--- src/graphics/MapCreation.java
+++ src/graphics/MapCreation.java
@@ -1,6 +1,11 @@
 package graphics;
 
 
+import javax.swing.text.html.parser.Entity;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class MapCreation {
@@ -13,8 +18,47 @@ public class MapCreation {
 
             StringTokenizer tokens = new StringTokenizer(line);
             _level = Integer.parseInt(tokens.nextToken());
-            _height = INteger.parseInt(tokens.nextToken());
-            //_width = Integer.parseInt(tokens.nextToken());
+            _height = Integer.parseInt(tokens.nextToken());
+            _width = Integer.parseInt(tokens.nextToken());
+
+            while (ip.hasNextLine()) {
+                idObjects = new int[_width][_height];
+                listKill = new int[_width][_height];
+                for (int i = 0; i < _height; ++i) {
+                    String lineTile = ip.nextLine();
+                    StringTokenizer tokenTile = new StringTokenizer(lineTile);
+
+                    for (int j = 0; j < _width; j++) {
+                        int token = Integer.parseInt(tokenTile.nextToken());
+                        Entity entity;
+
+                        switch (token) {
+                            case 1:
+                                entity = new Portal(j, i, Sprite.grass.getFxImage());
+                                token = 0;
+                                break;
+                            case 2:
+                                entity = new Wall(j, i, Sprite.wall.getFxImage());
+                                break;
+                            case 3:
+                                entity = new Brick(j, i, Sprite.brick.getFxImage());
+                                break;
+                            case 6:
+                                entity = new SpeedItem(j, i, Sprite.brick.getFxImage());
+                                break;
+                            case 7:
+                                entity = new FlameItem(j, i, Sprite.brick.getFxImage());
+                                break;
+                            default:
+                                entity = new Grass(j, i, Sprite.grass.getFxImage());
+                        }
+                        idObjects[j][i] = token;
+                        block.add(entity);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }

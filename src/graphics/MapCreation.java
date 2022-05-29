4ce69774ca36cb5d0866b61705df494dca5a8c01
Dang Nhat Huy <89309283+Nhathuy1305@users.noreply.github.com>
2022-05-29 16:54:48 +0700

    New branch creation

---
+++ src/graphics/MapCreation.java
@@ -0,0 +1,20 @@
+package graphics;
+
+
+import java.util.StringTokenizer;
+
+public class MapCreation {
+    public CreateMap (String level) {
+        System.out.println(System.getProperty(key:"user.dir"));
+        final File fileName = new File(level);
+        try (FileReader inputFile = new FileReader(fileName)) {
+            Scanner ip = new Scanner(inputFile);
+            String line = ip.nextLine();
+
+            StringTokenizer tokens = new StringTokenizer(line);
+            _level = Integer.parseInt(tokens.nextToken());
+            _height = INteger.parseInt(tokens.nextToken());
+            _width = Integer.parseInt(tokens.nextToken());
+        }
+    }
+}

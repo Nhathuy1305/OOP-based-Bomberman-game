d830bd0c597e95ea396dac50b502fc68abdde6e3
Công Bằng <92241569+congbangitiu@users.noreply.github.com>
2022-05-30 09:26:39 +0700

    Create Balloon enemy

---
+++ src/Entity/animal/Balloon.java
@@ -0,0 +1,76 @@
+package Entity.animal;
+
+import Entity.animal.Animal;
+import java.util.Random;
+
+public class Balloon extends Animal {
+    private static int swap_kill = 1;
+    private static int count_kill = 0;
+
+    public Balloon(int is_move, int swap, String direction, int count, int count_to_run) {
+        super(4, 1, "up", 0, 0);
+    }
+
+    public Balloon() {
+    }
+
+    private void killBalloon(Animal animal) {
+        if (count_kill % 16 == 0) {
+            if (swap_kill == 1) {
+                animal.setImg(Sprite.mob_dead1.getFxImage());
+                swap_kill = 2;
+            } else if (swap_kill == 2) {
+                animal.setImg(Sprite.mob_dead2.getFxImage());
+                swap_kill = 3;
+            } else if (swap_kill == 3) {
+                animal.setImg(Sprite.mob_dead3.getFxImage());
+                swap_kill = 4;
+            } else {
+                animal.setLife(false);
+                enemy.remove(animal);
+                swap_kill = 1;
+            }
+        }
+    }
+
+    private void kill() {
+        for (Animal animal : enemy) {
+            if (listKill[animal.getX() / 32][animal.getY() / 32] == 4) {
+                animal.setLife(false);
+            }
+        }
+    }
+
+    public Balloon(int x, int y, Image img) {
+        super(x, y, img);
+    }
+
+    @Override
+    public void update() {
+        kill();
+        count_kill++;
+        for (Animal animal : enemy) {
+            if (animal instanceof Balloon && !animal.life)
+                killBalloon(animal);
+        }
+
+        if (this.y % 16 == 0 && this.x % 16 == 0) {
+            Random random = new Random();
+            int direction = random.nextInt(4);
+            switch (direction) {
+                case 0:
+                    Move.down(this);
+                    break;
+                case 1:
+                    Move.up(this);
+                    break;
+                case 2:
+                    Move.left(this);
+                    break;
+                case 3:
+                    Move.right(this);
+                    break;
+            }
+        }
+    }
+}
---
+++ src/Entity/animal/Bomber.java
@@ -0,0 +1,108 @@
+package Entity.animal;
+
+import javafx.scene.image.Image;
+import Entity.animal.Animal;
+
+public class Bomber extends Animal {
+    public static int swap_kill = 1;
+    private static int count_kill = 0;
+
+    public Bomber(int is_move, int swap, String direction, int count, int count_to_run) {
+        super(8, 1, "down", 0, 0);
+    }
+
+    public Bomber() {
+    }
+
+    public Bomber(int x, int y, Image img) {
+        super(x, y, img);
+    }
+
+    private void killBomber(Animal animal) {
+        if (count_kill % 16 == 0) {
+            if (swap_kill == 1) {
+                animal.setImg(Sprite.player_dead1.getFxImage());
+                swap_kill = 2;
+            } else if (swap_kill == 2) {
+                animal.setImg(Sprite.player_dead2.getFxImage());
+                swap_kill = 3;
+            } else if (swap_kill == 3) {
+                animal.setImg(Sprite.player_dead3.getFxImage());
+                swap_kill = 4;
+            } else {
+                animal.setImg(Sprite.transparent.getFxImage());
+                running = false;
+                Image gameOver = new Image("images/gameOver.png");
+                authorView.setImage(gameOver);
+            }
+        }
+    }
+
+    private void checkBombs() {
+        if (listKill[player.getX() / 32][player.getY() / 32] == 4)
+            player.setLife(false);
+    }
+
+    private void checkEnemy1() {
+        int ax = player.getX() / 32;
+        int ay = player.getY() / 32;
+        for (Animal animal : enemy) {
+            int bx = animal.getX() / 32;
+            int by = animal.getY() / 32;
+            if (
+                ax == bx && ay == by
+                || ax == bx && ay == by + 1
+                || ax == bx && ay == by - 1
+                || ay == by && ax == bx + 1
+                || ay == by && ax == bx - 1
+            ) 
+            {
+                player.life = false;
+                break;
+            }
+        }
+    }
+
+    private void checkEnemy2() {    //easy level
+        int ax = player.getX();
+        int ay = player.getY();
+        for (Animal animal : enemy)
+            if (
+                ax == animal.getX() && ay == animal.getY()
+                || ax == animal.getX() && ay == animal.getY() - 32
+                || ax == animal.getX() && ay == animal.getY() + 32
+                || ay == animal.getY() && ax == animal.getX() - 32
+                || ay == animal.getY() && ax == animal.getX() + 32
+            )
+            {
+                player.life = false;
+                break;
+            }
+    }
+
+    private void checkEnemy3() {
+        int ax = player.getX();
+        int ay = player.getY();
+        for (Animal animal : enemy) {
+            int bx = animal.getX();
+            int by = animal.getY();
+            if (
+                ax == bx && by - 32 <= ay && by + 32 >= ay
+                || ay == by && bx - 32 <= ax && bx + 32 >= ax
+            ) 
+            {
+                player.life = false;
+                break;
+            }
+        }
+    }
+
+    @Override
+    public void update() {
+        checkBombs();
+        checkEnemy3();
+        count_kill++;
+        if (!player.life)
+            killBomber(player);
+    }
+}
